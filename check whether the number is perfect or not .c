9c59ef39c993901648d28fa129e61fd30946408e
karthik4178 <95008820+karthik4178@users.noreply.github.com>
2021-12-26 11:31:33 +0530

    Create check whether the number is perfect or not .c

---
+++ check whether the number is perfect or not .c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+int n,sum=0,i;
+printf("enter any number");
+scanf("%d",&n);
+for(i=1;i<n;i++)
+if(n=i+sum)
+    printf("enter the number is perfect number");
+else
+{
+    printf("enter the number is not a perfect number");
+}
+}
